299dd8564952862c773322f31cc29417bea576c6
DESKTOP-J1B1JIN\MCDsoftwares <berissocompratest@gmail.com>
2022-01-25 22:49:15 -0300

    third

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req, res)=>{
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.send('Hello word con express')
+    res.status(200).json({msg: 'Hello word con express'})
 })
 
 app.listen(3000);
